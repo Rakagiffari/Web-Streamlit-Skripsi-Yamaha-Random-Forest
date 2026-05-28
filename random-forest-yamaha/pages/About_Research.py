b411e479bbccd0c615d0936558ef8948bba970d8
Rakagiffari <rakagiffari945@gmail.com>
2026-05-28 16:54:22 +0700

    Update About_Research.py

--- random-forest-yamaha/pages/About_Research.py
+++ random-forest-yamaha/pages/About_Research.py
@@ -3,15 +3,10 @@ import streamlit as st
 st.title("📘 About Research")
 
 st.markdown("""
+
 ## Judul Penelitian
 
 Penerapan Algoritma Random Forest untuk Mengklasifikasi Layanan Servis pada Yamaha.
-
----
-
-## Tujuan Penelitian
-
-Membangun sistem machine learning untuk membantu klasifikasi layanan servis kendaraan Yamaha.
 
 ---
 
@@ -21,23 +16,11 @@ Membangun sistem machine learning untuk membantu klasifikasi layanan servis kend
 
 ---
 
-## Dataset
-
-Dataset layanan servis kendaraan Yamaha berbasis CSV.
-
----
-
-## Output Sistem
-
-- Service Ringan
-- Service Berat
-
----
-
 ## Tools
 
 - Python
 - Streamlit
 - Scikit-learn
 - Plotly
+
 """)
