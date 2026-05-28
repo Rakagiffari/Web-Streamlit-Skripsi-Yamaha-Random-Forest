bf3e9b0db25b2b55e1d901574317234eeb26a9a4
Rakagiffari <rakagiffari945@gmail.com>
2026-05-28 16:19:28 +0700

    Update Feature_Importance.py

--- random-forest-yamaha/pages/Feature_Importance.py
+++ random-forest-yamaha/pages/Feature_Importance.py
@@ -8,18 +8,30 @@ from utils.preprocessing import preprocess_data
 st.title("📈 Feature Importance")
 
 uploaded_file = st.file_uploader(
-    'Upload Dataset CSV',
+    "📂 Upload Dataset",
     type=['csv']
 )
 
 if uploaded_file:
 
+    # Membaca dataset
     df = pd.read_csv(uploaded_file)
 
+    # =========================
+    # PREPROCESSING
+    # =========================
     X, y, label_encoders, target_encoder = preprocess_data(df)
 
-    model = joblib.load('model/random_forest_model.pkl')
+    # =========================
+    # LOAD MODEL
+    # =========================
+    model = joblib.load(
+        'model/random_forest_model.pkl'
+    )
 
+    # =========================
+    # FEATURE IMPORTANCE
+    # =========================
     importance = pd.DataFrame({
         'Feature': X.columns,
         'Importance': model.feature_importances_
@@ -30,12 +42,18 @@ if uploaded_file:
         ascending=False
     )
 
+    # =========================
+    # VISUALISASI
+    # =========================
     fig = px.bar(
-        importance,
+        importance.head(10),
         x='Importance',
         y='Feature',
         orientation='h',
-        title='Feature Importance'
+        title='Top Feature Importance'
     )
 
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
