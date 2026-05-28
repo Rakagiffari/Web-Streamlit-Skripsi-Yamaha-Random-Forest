77a14007cae8d3fa47dcc0fc2cb5a8983234fa54
Rakagiffari <rakagiffari945@gmail.com>
2026-05-28 16:30:55 +0700

    Update Feature_Importance.py

--- random-forest-yamaha/pages/Feature_Importance.py
+++ random-forest-yamaha/pages/Feature_Importance.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import joblib
 
+from pathlib import Path
 from utils.preprocessing import preprocess_data
 
 st.title("📈 Feature Importance")
@@ -14,46 +15,55 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file:
 
-    # Membaca dataset
+    # =========================
+    # LOAD DATASET
+    # =========================
     df = pd.read_csv(uploaded_file)
 
-    # =========================
-    # PREPROCESSING
-    # =========================
     X, y, label_encoders, target_encoder = preprocess_data(df)
 
     # =========================
     # LOAD MODEL
     # =========================
-    model = joblib.load(
-        'model/random_forest_model.pkl'
-    )
+    BASE_DIR = Path(__file__).parent.parent
 
-    # =========================
-    # FEATURE IMPORTANCE
-    # =========================
-    importance = pd.DataFrame({
-        'Feature': X.columns,
-        'Importance': model.feature_importances_
-    })
+    model_path = BASE_DIR / "model" / "random_forest_model.pkl"
 
-    importance = importance.sort_values(
-        by='Importance',
-        ascending=False
-    )
+    if not model_path.exists():
 
-    # =========================
-    # VISUALISASI
-    # =========================
-    fig = px.bar(
-        importance.head(10),
-        x='Importance',
-        y='Feature',
-        orientation='h',
-        title='Top Feature Importance'
-    )
+        st.warning(
+            "Model belum tersedia. Silakan training model terlebih dahulu."
+        )
 
-    st.plotly_chart(
-        fig,
-        use_container_width=True
-    )
+    else:
+
+        model = joblib.load(model_path)
+
+        # =========================
+        # FEATURE IMPORTANCE
+        # =========================
+        importance = pd.DataFrame({
+            'Feature': X.columns,
+            'Importance': model.feature_importances_
+        })
+
+        importance = importance.sort_values(
+            by='Importance',
+            ascending=False
+        )
+
+        # =========================
+        # VISUALISASI
+        # =========================
+        fig = px.bar(
+            importance.head(10),
+            x='Importance',
+            y='Feature',
+            orientation='h',
+            title='Top Feature Importance'
+        )
+
+        st.plotly_chart(
+            fig,
+            use_container_width=True
+        )
