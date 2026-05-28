3def9df021be80939b4dfc19915395d3b877d9c0
Rakagiffari <rakagiffari945@gmail.com>
2026-05-28 19:24:21 +0700

    Update training.py

--- random-forest-yamaha/utils/training.py
+++ random-forest-yamaha/utils/training.py
@@ -1,32 +1,116 @@
-from sklearn.ensemble import RandomForestClassifier
+# =========================================
+# utils/training.py
+# =========================================
+
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import joblib
-import os
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import (
+
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+
+    precision_score,
+    recall_score,
+    f1_score
+)
 
 def train_model(X, y):
 
+    # =====================================
+    # SPLIT DATA
+    # =====================================
+
     X_train, X_test, y_train, y_test = train_test_split(
+
         X,
         y,
+
         test_size=0.2,
-        random_state=42
+
+        random_state=42,
+
+        stratify=y
     )
 
+    # =====================================
+    # RANDOM FOREST
+    # =====================================
+
     rf = RandomForestClassifier(
+
         n_estimators=300,
+
         max_depth=20,
-        random_state=42
+
+        min_samples_split=2,
+
+        min_samples_leaf=1,
+
+        class_weight='balanced',
+
+        random_state=42,
+
+        n_jobs=-1
     )
+
+    # =====================================
+    # TRAINING
+    # =====================================
 
     rf.fit(X_train, y_train)
 
+    # =====================================
+    # PREDIKSI
+    # =====================================
+
     y_pred = rf.predict(X_test)
 
-    accuracy = accuracy_score(y_test, y_pred)
+    # =====================================
+    # EVALUASI
+    # =====================================
 
-    os.makedirs("model", exist_ok=True)
+    accuracy = accuracy_score(
+        y_test,
+        y_pred
+    )
 
-    joblib.dump(rf, "model/random_forest.pkl")
+    precision = precision_score(
+        y_test,
+        y_pred
+    )
 
-    return rf, accuracy
+    recall = recall_score(
+        y_test,
+        y_pred
+    )
+
+    f1 = f1_score(
+        y_test,
+        y_pred
+    )
+
+    report = classification_report(
+        y_test,
+        y_pred
+    )
+
+    matrix = confusion_matrix(
+        y_test,
+        y_pred
+    )
+
+    return (
+
+        rf,
+
+        accuracy,
+        precision,
+        recall,
+        f1,
+
+        report,
+        matrix
+    )
