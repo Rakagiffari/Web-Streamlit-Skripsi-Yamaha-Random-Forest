05fa12a0d20744c93ac65f076667431f6346b874
Rakagiffari <rakagiffari945@gmail.com>
2026-06-04 11:30:37 +0700

    Update Home.py

--- random-forest-yamaha/Home.py
+++ random-forest-yamaha/Home.py
@@ -62,7 +62,7 @@ section[data-testid="stSidebar"]{
 ========================= */
 .main-title{
     text-align: center;
-    font-size: 50px;
+    font-size: 45px;
     font-weight: 900;
     color: white;
     line-height: 1.1;
