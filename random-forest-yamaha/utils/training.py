a41a3a6bd6f51631b910b5a64cc37993555a1c35
Rakagiffari <rakagiffari945@gmail.com>
2026-06-16 12:26:04 +0700

    Update training.py

--- random-forest-yamaha/utils/training.py
+++ random-forest-yamaha/utils/training.py
@@ -5,15 +5,12 @@
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-
 from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.metrics import (
-
     accuracy_score,
     classification_report,
     confusion_matrix,
-
     precision_score,
     recall_score,
     f1_score
@@ -28,7 +25,7 @@ def train_model(X, y):
 
     X = X.apply(
         pd.to_numeric,
-        errors='coerce'
+        errors="coerce"
     )
 
     X = X.fillna(0)
