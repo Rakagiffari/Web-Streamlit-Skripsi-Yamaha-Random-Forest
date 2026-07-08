9c85fef557802c045a33c3685a78bcd3b3aa7ffe
Rakagiffari <rakagiffari945@gmail.com>
2026-07-08 07:15:48 +0700

    Update training.py

--- random-forest-yamaha/utils/training.py
+++ random-forest-yamaha/utils/training.py
@@ -156,9 +156,8 @@ def train_model(X, y):
         "Indikasi": 0,
         "Km": 0,
         "Usia Motor": 0,
-        "Jenis": 0,
-        "Brand": 0
-
+        "Jenis": 0
+        
     }
 
     for _, row in importance.iterrows():
@@ -177,10 +176,6 @@ def train_model(X, y):
             grouped_importance["Usia Motor"] += nilai
 
         # KATEGORIKAL
-
-        elif fitur.startswith("Brand_"):
-
-            grouped_importance["Brand"] += nilai
 
         elif fitur.startswith("Jenis_"):
 
