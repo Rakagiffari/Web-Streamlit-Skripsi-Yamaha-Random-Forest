87b0f7334bde0ef2b50cc995c1e47a027ae1dd10
Rakagiffari <rakagiffari945@gmail.com>
2026-07-13 12:58:17 +0700

    Update Home.py

--- random-forest-yamaha/Home.py
+++ random-forest-yamaha/Home.py
@@ -650,46 +650,17 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown("""
-
-<div class="workflow-container">
-
-<div class="workflow-step">
-📂 Upload Dataset
-</div>
-
-<div class="workflow-arrow">
-⬇
-</div>
-
-<div class="workflow-step">
-⚙️ Preprocessing
-</div>
-
-<div class="workflow-arrow">
-⬇
-</div>
-
-<div class="workflow-step">
-🛠️ Proses
-</div>
-
-<div class="workflow-arrow">
-⬇
-</div>
-
-<div class="rf-container">
-
-<div class="rf-title">
-🌲 Random Forest
-</div>
-
-<div class="rf-card">
-🗂 Bootstrap Sampling
-</div>
-
-<div class="rf-center">
-⬇
-</div>
+    <div class="workflow-container">
+        <div class="rf-container">
+            <div class="rf-title">
+                🌲 Random Forest
+            </div>
+            <div class="rf-card"
+                🗂 Bootstrap Sampling
+            </div>
+            <div class="rf-center">
+                ⬇
+            </div>
 
 <div class="rf-card">
 📊 Gini Index
