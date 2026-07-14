57e5b8f4a30a4cc1187c29aa5712cad07c5cd928
Rakagiffari <rakagiffari945@gmail.com>
2026-07-14 20:47:30 +0700

    Update Proses.py

--- random-forest-yamaha/pages/Proses.py
+++ random-forest-yamaha/pages/Proses.py
@@ -8,8 +8,8 @@ from pathlib import Path
 
 from utils.preprocessing import preprocess_data
 from utils.training import train_model
-from sklearn.tree import plot_tree
 from utils.report import generate_pdf
+from sklearn.tree import plot_tree
 
 # =========================================
 # CONFIG
