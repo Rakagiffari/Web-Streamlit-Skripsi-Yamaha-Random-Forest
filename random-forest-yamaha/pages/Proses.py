b781de0e86e32a08fce58eb343a05f39e9bb7f23
Rakagiffari <rakagiffari945@gmail.com>
2026-07-16 12:02:54 +0700

    Update Proses.py

--- random-forest-yamaha/pages/Proses.py
+++ random-forest-yamaha/pages/Proses.py
@@ -211,17 +211,14 @@ if uploaded_file is not None:
 
             st.stop()
 
-                # =====================================
+        # =====================================
         # INFORMASI DATASET
         # =====================================
 
         c1, c2, c3 = st.columns(3)
-
         with c1:
-
             st.markdown(f"""
             <div class="upload-card">
-                <div class="upload-icon">📄</div>
                 <div class="upload-title">
                     Nama File
                 </div>
@@ -232,10 +229,8 @@ if uploaded_file is not None:
             """, unsafe_allow_html=True)
 
         with c2:
-
             st.markdown(f"""
             <div class="upload-card">
-                <div class="upload-icon">📊</div>
                 <div class="upload-title">
                     Jumlah Data
                 </div>
@@ -246,10 +241,8 @@ if uploaded_file is not None:
             """, unsafe_allow_html=True)
 
         with c3:
-
             st.markdown(f"""
             <div class="upload-card">
-                <div class="upload-icon">📋</div>
                 <div class="upload-title">
                     Jumlah Kolom
                 </div>
