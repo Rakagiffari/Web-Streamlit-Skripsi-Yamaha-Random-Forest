bca20382c8844e8cf90a2e090868383e12661b13
Rakagiffari <rakagiffari945@gmail.com>
2026-07-16 13:32:57 +0700

    Update Proses.py

--- random-forest-yamaha/pages/Proses.py
+++ random-forest-yamaha/pages/Proses.py
@@ -313,55 +313,6 @@ if uploaded_file is not None:
             )
 
         # =====================================
-        # SELEKSI FITUR
-        # =====================================
-
-        with st.expander("Seleksi Fitur", expanded=False):
-            fitur_digunakan = [
-                "Model",
-                "Tahun",
-                "Km",
-                "Indikasi",
-                "Service"
-            ]
-
-            fitur_dihapus = [
-                col
-                for col in df.columns
-                if col not in fitur_digunakan
-            ]
-
-            feature_info = pd.DataFrame({
-                "Nama":[
-                    "Fitur Digunakan",
-                    "Fitur Dihapus"
-                ],
-
-                "Jumlah":[
-                    len(fitur_digunakan),
-                    len(fitur_dihapus),
-                ]
-            })
-
-            st.dataframe(
-                feature_info,
-                use_container_width=True,
-                hide_index=True
-            )
-
-            st.caption("Dataset setelah seleksi fitur.")
-
-            df_selected = df[fitur_digunakan].copy()
-
-            st.dataframe(
-                df_selected,
-                use_container_width=True,
-                hide_index=True,
-                height=350
-            )
-            
-        
-        # =====================================
         # MISSING VALUE
         # =====================================
 
@@ -392,7 +343,7 @@ if uploaded_file is not None:
                 height=210
             )
 
-                # =====================================
+        # =====================================
         # DATA DUPLIKAT
         # =====================================
 
@@ -492,6 +443,54 @@ if uploaded_file is not None:
                 </div>
             </div>
             """, unsafe_allow_html=True)
+
+        # =====================================
+        # SELEKSI FITUR
+        # =====================================
+
+        with st.expander("Seleksi Fitur", expanded=False):
+            fitur_digunakan = [
+                "Model",
+                "Tahun",
+                "Km",
+                "Indikasi",
+                "Service"
+            ]
+
+            fitur_dihapus = [
+                col
+                for col in df.columns
+                if col not in fitur_digunakan
+            ]
+
+            feature_info = pd.DataFrame({
+                "Nama":[
+                    "Fitur Digunakan",
+                    "Fitur Dihapus"
+                ],
+
+                "Jumlah":[
+                    len(fitur_digunakan),
+                    len(fitur_dihapus),
+                ]
+            })
+
+            st.dataframe(
+                feature_info,
+                use_container_width=True,
+                hide_index=True
+            )
+
+            st.caption("Dataset setelah seleksi fitur.")
+
+            df_selected = df[fitur_digunakan].copy()
+
+            st.dataframe(
+                df_selected,
+                use_container_width=True,
+                hide_index=True,
+                height=350
+            )
 
         # =====================================
         # PREPROCESSING
