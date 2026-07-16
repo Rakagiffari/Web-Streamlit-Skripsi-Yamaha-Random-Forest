ac149a3d47d4c232d1c08ac241b46e8020ff898a
Rakagiffari <rakagiffari945@gmail.com>
2026-07-16 13:18:29 +0700

    Update Proses.py

--- random-forest-yamaha/pages/Proses.py
+++ random-forest-yamaha/pages/Proses.py
@@ -393,7 +393,7 @@ if uploaded_file is not None:
                 missing_df,
                 use_container_width=True,
                 hide_index=True,
-                height=200
+                height=230
             )
 
         # =====================================
