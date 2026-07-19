79d9c759314ffbafce72e6556e1f9e329d217275
Rakagiffari <rakagiffari945@gmail.com>
2026-07-19 20:13:10 +0700

    Update Proses.py

--- random-forest-yamaha/pages/Proses.py
+++ random-forest-yamaha/pages/Proses.py
@@ -7,7 +7,6 @@ import matplotlib.patches as patches
 import graphviz
 
 from pathlib import Path
-
 from utils.preprocessing import preprocess_data
 from utils.training import (
     train_model,
@@ -20,6 +19,7 @@ from sklearn.tree import (
 )
 from sklearn.tree import export_graphviz
 from utils.report import generate_pdf
+from utils.pdf_download import tampilkan_download_pdf
 
 # =========================================
 # CONFIG
@@ -1362,70 +1362,58 @@ Pada data yang digunakan belum ditemukan kendaraan jenis **{jenis}** yang dipred
             pdf_path = generate_pdf(
 
                 pdf_path=BASE_DIR / "laporan_training_model.pdf",
-
                 logo_path=logo_path,
-
                 total_data=len(df),
-
                 train_data=train_size,
-
                 test_data=test_size,
-
                 accuracy=accuracy,
-
                 precision=precision,
-
                 recall=recall,
-
                 f1=f1,
-
                 cm_image=cm_path,
-
                 fi_image=fi_path,
 
                 top_features=importance_grouped["Fitur"]
                     .head(5)
                     .tolist()
 
-            )
+                )
+
+            st.success("✅ Laporan PDF berhasil dibuat.")
+
+            st.markdown("<br>", unsafe_allow_html=True)
 
             left, center, right = st.columns([1, 2, 1])
 
             with center:
 
-                with open(pdf_path, "rb") as pdf_file:
+                with open(pdf_path, "rb") as pdf:
 
                     st.download_button(
 
                         label="📄 Download Laporan PDF",
 
-                        data=pdf_file.read(),
+                        data=pdf,
 
                         file_name="Laporan_Training_Model.pdf",
 
                         mime="application/pdf",
 
-                        use_container_width=True,
-
-                        key="download_pdf"
+                        use_container_width=True
 
                     )
 
             st.markdown(
                 """
-                <div style="
-                    text-align:center;
-                    color:#9ca3af;
-                    font-size:14px;
-                    margin-top:-8px;
-                    margin-bottom:15px;
-                ">
+                    <div style="
+                        text-align:center;
+                        color:#9ca3af;
+                        font-size:14px;
+                        margin-top:-8px;
+                        margin-bottom:25px;
+                    ">
                     Tekan tombol di atas untuk mengunduh laporan hasil pelatihan model dalam format PDF.
-                </div>
+                    </div>
                 """,
-                unsafe_allow_html=True
+            unsafe_allow_html=True
             )
-
-    except Exception as e:
-
-        st.exception(e)
