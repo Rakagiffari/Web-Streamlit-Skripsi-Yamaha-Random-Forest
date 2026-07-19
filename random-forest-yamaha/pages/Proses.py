17ba50f20167703e59d8412d8704735b7228ff9e
Rakagiffari <rakagiffari945@gmail.com>
2026-07-19 10:53:49 +0700

    Update Proses.py

--- random-forest-yamaha/pages/Proses.py
+++ random-forest-yamaha/pages/Proses.py
@@ -1265,7 +1265,7 @@ Representative Decision Path memberikan gambaran mengenai pola kendaraan yang pa
                     if data.empty:
                         continue
 
-                    st.markdown(f"{jenis}")
+                    st.info(f"{jenis}")
 
                     tampil = data[
                         [
