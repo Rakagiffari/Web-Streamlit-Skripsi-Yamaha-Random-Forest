2ab3868114c511f5c727c64189fb5fe48087828c
Rakagiffari <rakagiffari945@gmail.com>
2026-07-20 19:01:15 +0700

    Update report.py

--- random-forest-yamaha/utils/report.py
+++ random-forest-yamaha/utils/report.py
@@ -69,8 +69,8 @@ def generate_pdf(
 
     styles = getSampleStyleSheet()
 
-    # ==========================================================
-    # STYLE HEADER
+        # ==========================================================
+    # HEADER
     # ==========================================================
 
     company_style = ParagraphStyle(
@@ -80,7 +80,7 @@ def generate_pdf(
         fontSize=18,
         leading=22,
         alignment=TA_LEFT,
-        textColor=colors.black,
+        textColor=colors.black
     )
 
     subtitle_style = ParagraphStyle(
@@ -88,9 +88,9 @@ def generate_pdf(
         parent=styles["Normal"],
         fontName="Helvetica",
         fontSize=10,
-        leading=13,
+        leading=12,
         alignment=TA_LEFT,
-        textColor=colors.HexColor("#555555"),
+        textColor=colors.HexColor("#555555")
     )
 
     contact_style = ParagraphStyle(
@@ -100,7 +100,7 @@ def generate_pdf(
         fontSize=8.5,
         leading=11,
         alignment=TA_LEFT,
-        textColor=colors.HexColor("#666666"),
+        textColor=colors.HexColor("#666666")
     )
 
     date_style = ParagraphStyle(
@@ -108,7 +108,7 @@ def generate_pdf(
         parent=styles["Normal"],
         fontName="Helvetica",
         fontSize=10,
-        alignment=TA_RIGHT,
+        alignment=TA_RIGHT
     )
 
     report_title_style = ParagraphStyle(
@@ -117,13 +117,8 @@ def generate_pdf(
         fontName="Helvetica-Bold",
         fontSize=17,
         leading=20,
-        alignment=TA_CENTER,
-        textColor=colors.black,
+        alignment=TA_CENTER
     )
-
-    # ==========================================================
-    # HEADER
-    # ==========================================================
 
     # ==========================================================
     # LOGO
@@ -149,31 +144,31 @@ def generate_pdf(
         [
             [
                 Paragraph(
-                    "PT YAMAHA INDONESIA MOTOR MANUFUCTURING",
+                    "PT. YAMAHA TJAHAJA BARU TABING",
                     company_style
                 )
             ],
             [
                 Paragraph(
-                    "Yamaha Tjahaja Baru Tabing",
+                    "Laporan Hasil Sistem Klasifikasi Layanan Service Kendaraan",
                     subtitle_style
                 )
             ],
             [
                 Paragraph(
-                    "Jl. Prof. Dr. Hamka No.56, Parupuk Tabing, Kec. Koto Tangah, Kota Padang, Sumatera Barat 25173",
+                    "Jl. Adinegoro No.28, Tabing, Kota Padang",
                     contact_style
                 )
             ],
             [
                 Paragraph(
-                    "Telp : 08116606631",
+                    "Telp : (0751) 123456",
                     contact_style
                 )
             ],
             [
                 Paragraph(
-                    "Website : https://tjahaja-baru.com/",
+                    "Email : info@yamahatjahajabaru.co.id",
                     contact_style
                 )
             ]
