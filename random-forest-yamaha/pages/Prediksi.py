dc236602fb7e7e7ef7d84ba728ab4e17b227dee1
Rakagiffari <rakagiffari945@gmail.com>
2026-07-23 15:49:17 +0700

    Update Prediksi.py

--- random-forest-yamaha/pages/Prediksi.py
+++ random-forest-yamaha/pages/Prediksi.py
@@ -48,3 +48,115 @@ st.markdown(
 )
 
 st.markdown("---")
+
+# =========================================
+# MAPPING MODEL -> JENIS
+# =========================================
+
+def get_jenis(model):
+
+    model = model.upper()
+
+    if any(x in model for x in ["XMAX","NMAX","AEROX","LEXI","TMAX"]):
+        return "MAXi"
+
+    elif any(x in model for x in ["FAZZIO","FILANO"]):
+        return "Classy"
+
+    elif any(x in model for x in [
+        "MIO","SOUL","XEON","FINO",
+        "GEAR","FREEGO","X-RIDE",
+        "XRIDE","NOUVO","LEXAM"
+    ]):
+        return "Matic"
+
+    elif any(x in model for x in [
+        "R15","R25","R6","R1",
+        "VIXION","BYSON",
+        "SCORPIO","RX",
+        "XSR","MT"
+    ]):
+        return "Sport"
+
+    elif any(x in model for x in [
+        "WR","YZ"
+    ]):
+        return "Off-road"
+
+    elif any(x in model for x in [
+        "JUPITER","VEGA",
+        "CRYPTON","ALFA",
+        "SIGMA","F1ZR",
+        "MX KING"
+    ]):
+        return "Moped"
+
+    return "Unknown"
+
+col1, col2 = st.columns(2)
+with col1:
+
+    model = st.selectbox(
+        "Model Kendaraan",
+        [
+            "NMAX 155",
+            "AEROX 155",
+            "LEXI 125",
+            "XMAX 250",
+            "FAZZIO",
+            "GRAND FILANO",
+            "MIO M3",
+            "GEAR 125",
+            "FREEGO 125",
+            "VIXION",
+            "R15",
+            "WR155R",
+            "JUPITER Z1"
+        ]
+    )
+
+with col2:
+
+    jenis = get_jenis(model)
+
+    st.text_input(
+        "Jenis Motor",
+        value=jenis,
+        disabled=True
+    )
+
+col3, col4 = st.columns(2)
+with col3:
+
+    tahun = st.number_input(
+        "Tahun Motor",
+        min_value=2000,
+        max_value=2030,
+        value=2022
+    )
+with col4:
+
+    km = st.number_input(
+        "Kilometer",
+        min_value=0,
+        value=10000,
+        step=100
+    )
+
+indikasi = st.selectbox(
+    "Indikasi",
+    [
+        "Mesin",
+        "Transmisi",
+        "Kelistrikan",
+        "Pengereman",
+        "Roda dan Suspensi",
+        "Body",
+        "Umum"
+    ]
+)
+
+st.button(
+    "🔍 Prediksi Layanan",
+    use_container_width=True
+)
